1e3c3d0cd4474510f24334ca2202d7289eee20f0
Libb521 <eotieno39@yahoo.com>
2020-03-15 12:19:55 +0300

    created my first model called Profile that is to store the user's profile pic, bio and User_id

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -4,3 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images/')
+    bio = models.TextField(blank=True)
+    user_id = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    
