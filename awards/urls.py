a37f32ec8e04aab4fc970ceb872a02f24ca16b9b
Libb521 <eotieno39@yahoo.com>
2020-03-15 11:13:23 +0300

    customized the navbar to include the login and sign-up button

--- awards/urls.py
+++ awards/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from awardsapp import views as awardsapp_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', awardsapp_views.signup,name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout'),
 ]
